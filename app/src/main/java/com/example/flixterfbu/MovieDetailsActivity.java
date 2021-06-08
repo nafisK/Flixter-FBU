cdfdecf94100c9601188ead1d6c64ff001ed7dfb
Nafis Khan <nafisrizwank@gmail.com>
2021-06-08 18:18:53 -0400

    Final Commit

--- app/src/main/java/com/example/flixterfbu/MovieDetailsActivity.java
+++ app/src/main/java/com/example/flixterfbu/MovieDetailsActivity.java
@@ -2,12 +2,21 @@ package com.example.flixterfbu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterInside;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.example.flixterfbu.databinding.ActivityMainBinding;
 import com.example.flixterfbu.models.Movie;
 
@@ -24,20 +33,22 @@ public class MovieDetailsActivity extends AppCompatActivity {
     TextView tvTitle;
     TextView tvOverview;
     RatingBar rbVoteAverage;
+    ImageView posterBackdrop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_movie_details);
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
-        // layout of activity is stored in a special property called root
-        View view = binding.getRoot();
-        setContentView(view);
+        setContentView(R.layout.activity_movie_details);
 
         // resolve the view objects
         tvTitle = (TextView) findViewById(R.id.tvTitle);
         tvOverview = (TextView) findViewById(R.id.tvOverview);
         rbVoteAverage = (RatingBar) findViewById(R.id.rbVoteAverage);
+        posterBackdrop = (ImageView) findViewById(R.id.posterBackdrop);
+
+        LayerDrawable stars = (LayerDrawable) rbVoteAverage.getProgressDrawable();
+        stars.getDrawable(2).setColorFilter(Color.YELLOW, PorterDuff.Mode.SRC_ATOP);
+
 
         // unwrap the movie passed in via intent, using its simple name as a key
         movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra("movie"));
@@ -45,10 +56,32 @@ public class MovieDetailsActivity extends AppCompatActivity {
         // set the title and overview
         tvTitle.setText(movie.getTitle());
         tvOverview.setText(movie.getOverview());
+        String imageUrl = movie.getBackdrop_path();
+        Glide.with(this).load(imageUrl).
+                transform(new CenterInside(), new RoundedCorners(24)).
+                placeholder(R.drawable.movie_placeholder_vertical).
+                into(posterBackdrop);
 
         // vote average is 0..10, convert to 0..5 by dividing by 2
         float voteAverage = movie.getVote_average().floatValue();
         rbVoteAverage.setRating(voteAverage = voteAverage > 0 ? voteAverage / 2.0f : voteAverage);
+
+        posterBackdrop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (movie.getId() == null) {
+                    Toast.makeText(MovieDetailsActivity.this, "Error. Cannot find movie.", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Intent i = new Intent(MovieDetailsActivity.this, MovieTrailerActivity.class);
+                    i.putExtra("id", movie.getId());
+                    startActivity(i);
+                }
+            }
+        });
+
+
     }
 
 
---
+++ app/src/main/java/com/example/flixterfbu/MovieTrailerActivity.java
@@ -0,0 +1,85 @@
+package com.example.flixterfbu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.flixterfbu.databinding.ActivityMovieTrailerBinding;
+import com.example.flixterfbu.models.Movie;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import okhttp3.Headers;
+
+public class MovieTrailerActivity extends YouTubeBaseActivity {
+
+    ActivityMovieTrailerBinding binding;
+    public static final String TAG = "MovieTrailerActivity";
+    String videoKey;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMovieTrailerBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        // temporary test video id -- TODO replace with movie trailer video id
+        String videoId = getIntent().getStringExtra("id");
+        String URL = String.format("https://api.themoviedb.org/3/movie/%s/videos?api_key=a84a9bec1794546328acf362d75d590c", videoId);
+
+        AsyncHttpClient client  = new AsyncHttpClient();
+        client.get(URL, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int i, Headers headers, JSON json) {
+                Log.d(TAG, "onSuccess");
+                JSONObject jsonObject = json.jsonObject;
+
+                try {
+                    JSONArray results = jsonObject.getJSONArray("results");
+                    JSONObject object = results.getJSONObject(0);
+                    videoKey = object.getString("key");
+                } catch (JSONException e) {
+                    Log.e(TAG, "Hit json exception", e);
+                }
+            }
+
+            @Override
+            public void onFailure(int i, Headers headers, String s, Throwable throwable) {
+                Log.e(TAG, throwable.toString());
+            }
+        });
+
+
+
+        // resolve the player view from the layout
+        YouTubePlayerView playerView = (YouTubePlayerView) findViewById(R.id.player);
+
+        // initialize with API key stored in secrets.xml
+        playerView.initialize(getString(R.string.youtube_api_key), new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider,
+                                                YouTubePlayer youTubePlayer, boolean b) {
+                // do any work here to cue video, play video, etc.
+                youTubePlayer.cueVideo(videoKey);
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                                YouTubeInitializationResult youTubeInitializationResult) {
+                // log the error
+                Log.e("MovieTrailerActivity", "Error initializing YouTube player: " + youTubeInitializationResult);
+            }
+        });
+    }
+}
